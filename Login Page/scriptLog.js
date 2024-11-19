8da8067d0c41308f953f562a1111d0e9a9510ef7
TheLonelySamurai <yayasanouvong@gmail.com>
2024-11-19 13:30:24 -0500

    fixed animation

--- Login Page/scriptLog.js
+++ Login Page/scriptLog.js
@@ -1,7 +1,7 @@
 const popup = document.getElementById('popup');
-  popup.classList.remove('hidden'); // Show the popup
+  popup.classList.add('popup-show'); // Show the popup
 
   // Hide popup after 3 seconds and redirect to login page
   setTimeout(() => {
-    popup.classList.add('hidden'); // Hide the popup
+    popup.classList.remove('popup-show'); // Hide the popup
   }, 3000);
